ad5ade0261c9b8e614b164ed1ca2345039d078bc
ryansummerlin <ryansummerlin777@gmail.com>
2023-05-14 17:38:30 -0700

    Created bonus path, guessed html address correctly

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-// Your code here
 
-const port = 5000;
+// I think the route for this should be 'localhost:5001/stickers/hello.png
+app.use('/stickers', express.static('assets/images'));
+
+const port = 5001;
 app.listen(port, () => console.log('Server is listening on port', port));
